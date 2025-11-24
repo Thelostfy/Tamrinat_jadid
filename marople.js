d20a325aecc91cc7d3df5f772addc172c06d5f65
aryan salehi <1arian.salehi1389@gmail.com>
2025-11-24 12:02:55 +0330

    Maro-Ple

---
+++ marople.js
@@ -0,0 +1,43 @@
+let gameboard = document.getElementById("GameBoard");
+
+
+
+let CreateTable = () => {
+    for (let i = 0; i < 10; i++) {
+
+        let createTr = document.createElement("tr");
+        createTr.className = 'TRClass';
+
+        for (let j = 0; j < 10; j++) {
+
+            let createTd = document.createElement("td");
+            createTd.className = "TdClass";
+            let Id = `${i}-${j}`;// i شده سطر و جی شده ستون 
+            createTd.id = Id;       
+
+            // let zoj = i / 2 === 0;
+            // if ((i+j) % 2 === 0) {
+            //     createTd.style.backgroundColor = "blue";
+            // }
+            // else(
+            //     createTd.style.backgroundColor = "red"
+
+            // )
+            let mouseHover = createTd.addEventListener("mouseover", ()=>{
+                createTd.style.backgroundColor = "white";
+                // createTr.innerText = Id;
+            })
+            let mouseOutRang = createTd.addEventListener("mouseout", ()=>{
+                createTd.style.backgroundColor = "";
+                
+            })
+
+            createTr.appendChild(createTd);
+        }
+
+
+
+        gameboard.appendChild(createTr);
+    }
+}
+CreateTable()
